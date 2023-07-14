91f901b8636a340f98f57bddffeea8f1355819a4
Lirovsk <joao.lira@engenharia.ufjf.br>
2023-07-14 14:01:56 -0300

    Ultima modificacao antes do teste

--- constantes.py
+++ constantes.py
@@ -6,6 +6,7 @@ def add_lookup(cls):
     setattr(cls, 'lookup', lookup)
     return cls
 
+
 @add_lookup
 class MODE:
     SLEEP    = 0x80
@@ -16,7 +17,79 @@ class MODE:
     RXCONT   = 0x85
     RXSINGLE = 0x86
     CAD      = 0x87
-    FSK_STDBY= 0x01 
+    FSK_STDBY= 0x01     # needed for calibration
+
+
+@add_lookup
+class BW:
+    BW7_8   = 0
+    BW10_4  = 1
+    BW15_6  = 2
+    BW20_8  = 3
+    BW31_25 = 4
+    BW41_7  = 5
+    BW62_5  = 6
+    BW125   = 7
+    BW250   = 8
+    BW500   = 9
+
+
+@add_lookup
+class CODING_RATE:
+    CR4_5 = 1
+    CR4_6 = 2
+    CR4_7 = 3
+    CR4_8 = 4
+
+
+@add_lookup
+class GAIN:
+    NOT_USED = 0b000
+    G1       = 0b001
+    G2       = 0b010
+    G3       = 0b011
+    G4       = 0b100
+    G5       = 0b101
+    G6       = 0b110
+
+
+@add_lookup
+class PA_SELECT:
+    RFO      = 0
+    PA_BOOST = 1
+
+
+@add_lookup
+class PA_RAMP:
+    RAMP_3_4_ms = 0
+    RAMP_2_ms   = 1
+    RAMP_1_ms   = 2
+    RAMP_500_us = 3
+    RAMP_250_us = 4
+    RAMP_125_us = 5
+    RAMP_100_us = 6
+    RAMP_62_us  = 7
+    RAMP_50_us  = 8
+    RAMP_40_us  = 9
+    RAMP_31_us  = 10
+    RAMP_25_us  = 11
+    RAMP_20_us  = 12
+    RAMP_15_us  = 13
+    RAMP_12_us  = 14
+    RAMP_10_us  = 15
+
+
+class MASK:
+    class IRQ_FLAGS:
+        RxTimeout           = 7
+        RxDone              = 6
+        PayloadCrcError     = 5
+        ValidHeader         = 4
+        TxDone              = 3
+        CadDone             = 2
+        FhssChangeChannel   = 1
+        CadDetected         = 0
+
 
 class REG:
 
@@ -70,3 +143,25 @@ class REG:
         AGC_THRESH_2       = 0x63
         AGC_THRESH_3       = 0x64
         PLL                = 0x70
+
+    @add_lookup
+    class FSK:
+        LNA                = 0x0C
+        RX_CONFIG          = 0x0D
+        RSSI_CONFIG        = 0x0E
+        PREAMBLE_DETECT    = 0x1F
+        OSC                = 0x24
+        SYNC_CONFIG        = 0x27
+        SYNC_VALUE_1       = 0x28
+        SYNC_VALUE_2       = 0x29
+        SYNC_VALUE_3       = 0x2A
+        SYNC_VALUE_4       = 0x2B
+        SYNC_VALUE_5       = 0x2C
+        SYNC_VALUE_6       = 0x2D
+        SYNC_VALUE_7       = 0x2E
+        SYNC_VALUE_8       = 0x2F
+        PACKET_CONFIG_1    = 0x30
+        FIFO_THRESH        = 0x35
+        IMAGE_CAL          = 0x3B
+        DIO_MAPPING_1      = 0x40
+        DIO_MAPPING_2      = 0x41
