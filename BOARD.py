5e8474e2aa0c3fec13fc2e4e0fe7fabec177acd5
Lirovsk <joao.lira@engenharia.ufjf.br>
2023-08-21 18:30:44 -0300

    ....

--- BOARD.py
+++ BOARD.py
@@ -18,9 +18,9 @@ class BOARD:
     LED  = 13   # RaspPi GPIO 13 connects to the LED and a resistor (1kohm or 330ohm)
     #SWITCH = 4  # RaspPi GPIO 4 connects to a switch - not necessary
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(4 , input)
-    GPIO.setup(17 , input)
-    GPIO.setup(18 , input)
+    GPIO.setup(4 , GPIO.IN)
+    GPIO.setup(17 , GPIO.IN)
+    GPIO.setup(18 , GPIO.IN)
     # The spi object is kept here
     spi = None
     SPI_BUS=0
